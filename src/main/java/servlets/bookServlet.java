bbf0ad55e680ababf686a22ccd2c65fd3ce8001c
TimelesClock <leongandy027@gmail.com>
2023-06-14 23:22:16 +0800

    push

--- src/main/java/servlets/bookServlet.java
+++ src/main/java/servlets/bookServlet.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
